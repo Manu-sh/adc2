13415811b0166dc95fb4126801b0c2e9c01484b4
manu-sh <s3gmentationfault@gmail.com>
2019-12-04 16:32:16 +0100

    --

--- adc2.c
+++ adc2.c
@@ -20,6 +20,7 @@ typedef struct {
 #include "my_collections/src/inlined/vector/vector_x32.h"
 
 
+// TODO: me li sto leggendo 4 alla volta, il res è una mtx a me serve un array 1d
 static inline bool x32_read(x32 *slice, FILE *stream) {
 	return fscanf(stream, "%"SCNd32"," "%"SCNd32"," "%"SCNd32"," "%"SCNu32",", &slice->op, &slice->a, &slice->b, &slice->where) == 4;
 }
@@ -32,18 +33,32 @@ static inline const x32 * x32_run(x32 *vct, size_t len, const x32 *slice) {
 
 	switch (slice->op) {
 		case ADD:
-			assert(slice->where < len * sizeof(int32_t));
-			((int32_t *)vct)[slice->where] = slice->a + slice->b;
-			return slice;
+			{
+				assert(slice->where < len * sizeof(int32_t));
+				assert(slice->a < len * sizeof(int32_t));
+				assert(slice->b < len * sizeof(int32_t));
+				int32_t *mem32 = (int32_t *)vct;
+				mem32[slice->where] = mem32[slice->a] + mem32[slice->b];
+				exit(1);
+				return slice;
+			}
 		case MUL:
-			assert(slice->where < len * sizeof(int32_t));
-			((int32_t *)vct)[slice->where] = slice->a * slice->b;
-			return slice;
+			{
+				assert(slice->where < len * sizeof(int32_t));
+				assert(slice->a < len * sizeof(int32_t));
+				assert(slice->b < len * sizeof(int32_t));
+				int32_t *mem32 = (int32_t *)vct;
+				mem32[slice->where] = mem32[slice->a] + mem32[slice->b];
+				((int32_t *)vct)[slice->where] = slice->a + slice->b;
+				((int32_t *)vct)[slice->where] = slice->a * slice->b;
+				return slice;
+			}
 		case DIE:
 			// TODO: here
 			for (int i = 0; i < len; i++)
 				x32_write(vct + i, stdout);
-			return NULL;
+			exit(0);
+			// return NULL;
 	}
 
 	assert(0);
@@ -71,8 +86,14 @@ int main() {
 	const int len = vector_x32_length(vct);
 
 	// run
-	for (int i = 0; i < len && x32_run(raw_vct, len, raw_vct + i); i++)
-		;
+	// for (int i = 0; i < len && x32_run(raw_vct, len, raw_vct + i); i++)
+	//	;
+
+
+	// printf("->%d<-", raw_vct[3]);
+	printf("->%d<-", sizeof(*raw_vct));
+	printf("->%d<-", raw_vct[2]);
+
 
 	for (int i = 0; i < len; i++)
 		x32_write(raw_vct + i, stdout);
