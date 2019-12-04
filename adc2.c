4f1fbcef1a248e1e02599e518d4cc67a9df21b0e
manu-sh <s3gmentationfault@gmail.com>
2019-12-04 13:55:38 +0100

    fixs

--- adc2.c
+++ adc2.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <inttypes.h>
 #include <stdbool.h>
 #include <assert.h>
 
@@ -11,29 +12,30 @@ typedef enum {
 } OP;
 
 typedef struct {
-	uint8_t op;
+	int32_t op;
 	int32_t a;
 	int32_t b;
-	int32_t x;
-} x32;
+	uint32_t where;
+} x32; // TODO: no padding
 
 static inline bool x32_read(x32 *slice, FILE *stream) {
-	return fscanf(stream, "%hhu, %d, %d, %d", &slice->op, &slice->a, &slice->b, &slice->x) == 4;
+	return fscanf(stream, "%"SCNd32"," "%"SCNd32"," "%"SCNd32"," "%"SCNu32",", &slice->op, &slice->a, &slice->b, &slice->where) == 4;
 }
 
 static inline bool x32_write(const x32 *slice, FILE *stream) {
-	fprintf(stream, "%hhu, %d, %d, %d", slice->op, slice->a, slice->b, slice->x);
-	return 1;
+	return fprintf(stream, "%"PRId32"," "%"PRId32"," "%"PRId32"," "%"PRIu32",", slice->op, slice->a, slice->b, slice->where), true;
 }
 
-static inline x32 * x32_run(x32 *slice) {
+static inline const x32 * x32_run(x32 *vct, size_t len, const x32 *slice) {
 
 	switch (slice->op) {
 		case ADD:
-			slice->x = slice->a + slice->b;
+			assert(slice->where < len * sizeof(int32_t));
+			((int32_t *)vct)[slice->where] = slice->a + slice->b;
 			return slice;
 		case MUL:
-			slice->x = slice->a * slice->b;
+			assert(slice->where < len * sizeof(int32_t));
+			((int32_t *)vct)[slice->where] = slice->a * slice->b;
 			return slice;
 		case DIE:
 			return NULL;
@@ -46,7 +48,9 @@ static inline x32 * x32_run(x32 *slice) {
 int main() {
 
 	x32 slice;
-	while (x32_read(&slice, stdin) && x32_run(&slice))
+	x32 *slice_vct = &slice;
+
+	while (x32_read(&slice, stdin) && x32_run(slice_vct, 1, &slice))
 		x32_write(&slice, stdout);
 
 	return 0;
