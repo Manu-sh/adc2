132deb79e5af2b681c2ba78887f3560348577809
manu-sh <s3gmentationfault@gmail.com>
2019-12-05 13:52:44 +0100

    p2

--- adc2.c
+++ adc2.c
@@ -83,33 +83,48 @@ static inline void mx32_unload(vector_w32 *mem) {
 
 int main() {
 
-	vector_w32 *vct = mx32_load("programs.txt");
-	assert(sizeof(x32) == 4 * sizeof(w32));
-	assert(vct);
+	for (int a = 0; a <= 99; a++) {
+		for (int b = 0; b <= 99; b++) {
 
-	const int len = vector_w32_length(vct);
-	w32 *raw_vct  = vector_w32_data(vct);
+			vector_w32 *vct = mx32_load("programs.txt");
+			assert(sizeof(x32) == 4 * sizeof(w32));
+			assert(vct);
 
-	assert(len > 0);
+			const int len = vector_w32_length(vct);
+			w32 *raw_vct  = vector_w32_data(vct);
 
-	raw_vct[1] = 12;
-	raw_vct[2] = 2;
+			assert(len > 0);
 
-	// run
-	for (int_fast32_t i = 0; i < len; i += sizeof(x32) / sizeof(w32)) {
-		const x32 *slice = (const x32 *)(raw_vct + i);
-		if (!x32_run(raw_vct, len, slice))
-			break;
+			raw_vct[1] = a;
+			raw_vct[2] = b;
+
+			// run
+			for (int_fast32_t i = 0; i < len; i += sizeof(x32) / sizeof(w32)) {
+				const x32 *slice = (const x32 *)(raw_vct + i);
+				if (!x32_run(raw_vct, len, slice))
+					break;
+			}
+
+			{ // dump
+				int_fast32_t i;
+				for (i = 1; i < len && raw_vct[i] != W32_INVALID; i++)
+					fprintf(stdout, "%"PRIu32",", raw_vct[i - 1]);
+
+				fprintf(stdout, "%"PRIu32"\n", raw_vct[i-1]);
+			}
+
+			if (raw_vct[0] == 19690720) {
+				printf("%d-> a=%d b=%d\n", raw_vct[0], a, b);
+				printf("%d-> v[a]=%d v[b]=%d\n", raw_vct[0], raw_vct[a], raw_vct[b]);
+
+				printf("%d-> 100 * a=%d + b=%d = %d\n", raw_vct[0], a, b, 100 * a + b);
+				printf("%d-> 100 * v[a]=%d + v[b]=%d = %d\n", raw_vct[0], raw_vct[a], raw_vct[b], 100 * raw_vct[a] + raw_vct[b]);
+				a = b = 100; // brk
+			}
+
+			mx32_unload(vct);
+		}
 	}
 
-	{ // dump
-		int_fast32_t i;
-		for (i = 1; i < len && raw_vct[i] != W32_INVALID; i++)
-			fprintf(stdout, "%"PRIu32",", raw_vct[i - 1]);
-
-		fprintf(stdout, "%"PRIu32"\n", raw_vct[i-1]);
-	}
-
-	mx32_unload(vct);
 	return 0;
 }
