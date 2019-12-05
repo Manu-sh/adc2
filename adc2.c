295ad39db208cc4e7ba57581bb07990976577b11
manu-sh <s3gmentationfault@gmail.com>
2019-12-05 15:20:18 +0100

    p2 - refactor

--- adc2.c
+++ adc2.c
@@ -9,12 +9,10 @@ typedef uint32_t w32; // word32
 #include "my_collections/src/inlined/vector/vector_w32.h"
 
 #define W32_INVALID UINT32_MAX
-
-enum {
-	ADD = 1,
-	MUL,
-	DIE = 99
-};
+#define ADD 1
+#define MUL 2
+#define DIE 99
+#define CHUNK_LEN (sizeof(x32) / sizeof(w32))
 
 // ensure that there is no padding here
 typedef struct {
@@ -22,7 +20,7 @@ typedef struct {
 	w32 a;
 	w32 b;
 	w32 where;
-} __attribute__((__packed__)) x32;
+} __attribute__((aligned( sizeof(w32) ))) __attribute__((__packed__)) x32;
 
 
 static inline bool x32_read(x32 *const restrict slice, FILE *stream) {
@@ -30,25 +28,25 @@ static inline bool x32_read(x32 *const restrict slice, FILE *stream) {
 	const int ret = fscanf(stream, "%"SCNu32"," "%"SCNu32"," "%"SCNu32"," "%"SCNu32",", &slice->op, &slice->a, &slice->b, &slice->where);
 
 	if (ret == EOF) return false;
-	for (int_fast8_t x = sizeof(x32) / sizeof(w32) -1; x >= ret; --x)
+	for (int_fast8_t x = CHUNK_LEN-1; x >= ret; --x)
 		((w32 *)slice)[x] = W32_INVALID;
 
 	return true;
 }
 
-static inline const x32 * x32_run(w32 *const mem32, size_t len, const x32 *const slice) {
+static inline const x32 * x32_run(w32 *const mem32, int_fast32_t len, const x32 *const slice) {
 
 	switch (slice->op) {
 		case ADD:
-			assert((size_t)slice->where < len);
-			assert((size_t)slice->a     < len);
-			assert((size_t)slice->b     < len);
+			assert(slice->where < len);
+			assert(slice->a     < len);
+			assert(slice->b     < len);
 			mem32[slice->where] = mem32[slice->a] + mem32[slice->b];
 			return slice;
 		case MUL:
-			assert((size_t)slice->where < len);
-			assert((size_t)slice->a     < len);
-			assert((size_t)slice->b     < len);
+			assert(slice->where < len);
+			assert(slice->a     < len);
+			assert(slice->b     < len);
 			mem32[slice->where] = mem32[slice->a] * mem32[slice->b];
 			return slice;
 		case DIE:
@@ -60,18 +58,12 @@ static inline const x32 * x32_run(w32 *const mem32, size_t len, const x32 *const
 
 static inline vector_w32 * mx32_load(const char *fname) {
 
-	assert(sizeof(x32) == 4 * sizeof(w32));
-
 	FILE *fp = fopen(fname, "r");
 	vector_w32 *vct = vector_w32_new();
 	assert(fp);
 
-	for (x32 slice; x32_read(&slice, fp);) {
-		vector_w32_pushBack(vct, slice.op);
-		vector_w32_pushBack(vct, slice.a);
-		vector_w32_pushBack(vct, slice.b);
-		vector_w32_pushBack(vct, slice.where);
-	}
+	for (x32 slice; x32_read(&slice, fp);)
+		vector_w32_pushBackAll(vct, (w32 *)&slice, CHUNK_LEN);
 
 	return fclose(fp), vct;
 }
@@ -80,51 +72,62 @@ static inline void mx32_unload(vector_w32 *mem) {
 	vector_w32_free(mem);
 }
 
+static inline void mx32_dump(const vector_w32 *mem) {
+
+	const w32 *const raw_vct = vector_w32_data(mem);
+	const int_fast32_t len = vector_w32_length(mem);
+	int_fast32_t i;
+
+	for (i = 1; i < len && raw_vct[i] != W32_INVALID; i++)
+		fprintf(stdout, "%"PRIu32",", raw_vct[i - 1]);
+
+	fprintf(stdout, "%"PRIu32"\n", raw_vct[i - 1]);
+}
+
+static inline void mx32_exec(vector_w32 *mem) {
+
+	w32 *const raw_mem = vector_w32_data(mem);
+	const int_fast32_t len = vector_w32_length(mem);
+
+	for (int_fast32_t i = 0; i < len; i += CHUNK_LEN) {
+		const x32 *slice = (const x32 *)(raw_mem + i);
+		if (!x32_run(raw_mem, len, slice))
+			break;
+	}
+}
 
 int main() {
 
-	for (int a = 0; a <= 99; a++) {
-		for (int b = 0; b <= 99; b++) {
+	vector_w32 *restrict const vct = mx32_load("program.txt");
+	const int_fast32_t len = vector_w32_length(vct);
+	const w32 *restrict const raw_vct  = vector_w32_data(vct);
+	assert(len > 0);
 
-			vector_w32 *vct = mx32_load("programs.txt");
-			assert(sizeof(x32) == 4 * sizeof(w32));
-			assert(vct);
+	for (int_fast8_t a = 0; a <= 99; a++) {
+		for (int_fast8_t b = 0; b <= 99; b++) {
 
-			const int len = vector_w32_length(vct);
-			w32 *raw_vct  = vector_w32_data(vct);
+			// mx32_copy
+			vector_w32 *mem = vector_w32_new();
+			vector_w32_pushBackAll(mem, raw_vct, len);
+			w32 *raw_mem = vector_w32_data(mem);
 
-			assert(len > 0);
+			assert(mem);
 
-			raw_vct[1] = a;
-			raw_vct[2] = b;
+			raw_mem[1] = a;
+			raw_mem[2] = b;
 
-			// run
-			for (int_fast32_t i = 0; i < len; i += sizeof(x32) / sizeof(w32)) {
-				const x32 *slice = (const x32 *)(raw_vct + i);
-				if (!x32_run(raw_vct, len, slice))
-					break;
-			}
+			mx32_exec(mem);
+			// mx32_dump(mem);
 
-			{ // dump
-				int_fast32_t i;
-				for (i = 1; i < len && raw_vct[i] != W32_INVALID; i++)
-					fprintf(stdout, "%"PRIu32",", raw_vct[i - 1]);
-
-				fprintf(stdout, "%"PRIu32"\n", raw_vct[i-1]);
-			}
-
-			if (raw_vct[0] == 19690720) {
-				printf("%d-> a=%d b=%d\n", raw_vct[0], a, b);
-				printf("%d-> v[a]=%d v[b]=%d\n", raw_vct[0], raw_vct[a], raw_vct[b]);
-
-				printf("%d-> 100 * a=%d + b=%d = %d\n", raw_vct[0], a, b, 100 * a + b);
-				printf("%d-> 100 * v[a]=%d + v[b]=%d = %d\n", raw_vct[0], raw_vct[a], raw_vct[b], 100 * raw_vct[a] + raw_vct[b]);
+			if (raw_mem[0] == 19690720) {
+				printf("%d\n", 100 * a + b);
 				a = b = 100; // brk
 			}
 
-			mx32_unload(vct);
+			vector_w32_free(mem);
 		}
 	}
 
+	mx32_unload(vct);
 	return 0;
 }
